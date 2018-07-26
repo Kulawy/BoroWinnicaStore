92c90492691b8fb0c58e967edf59b0cac30c0487
Kulawy <catatau1992@gmail.com>
2018-07-26 17:45:12 +0200

    Initial solution

---
+++ BoroWinnicaStore.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BoroWinnicaStore.WebUI.MvcApplication" Language="C#" %>
---
+++ BoroWinnicaStore.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoroWinnicaStore.WebUI.Startup))]
+namespace BoroWinnicaStore.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
