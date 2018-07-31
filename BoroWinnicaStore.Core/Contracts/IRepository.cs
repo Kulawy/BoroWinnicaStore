606f558f8cc959c9456fc775e837f1c567d5c377
Kulawy <catatau1992@gmail.com>
2018-07-31 10:21:59 +0200

    Models Implemented.

---
+++ BoroWinnicaStore.Core/Contracts/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using BoroWinnicaStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoroWinnicaStore.Core.Contracts
+{
+    public interface IRepository<T> where T: BaseEntity
+    {
+        IQueryable<T> Collection();
+        void Comit();
+        void Delete(string Id);
+        T Find(string Id);
+        void Insert(T t);
+        void Update(T t);
+        
+    }
+}
